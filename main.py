4e838732f674eeb6fdd7a4c6e967cbf67af36744
laszlonaptar <bloffolo_szuleszno2v@icloud.com>
2025-08-05 15:20:38 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,10 +1,42 @@
-from flask import Flask, render_template
 
-app = Flask(__name__, static_folder='static', template_folder='templates')
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import FileResponse
+import openpyxl
+from datetime import datetime
+import os
 
-@app.route('/')
-def index():
-    return render_template('index.html')
+app = FastAPI()
 
-if __name__ == '__main__':
-    app.run(debug=True)
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
+
+@app.get("/", response_class=HTMLResponse)
+async def read_root(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+@app.post("/generate_excel/")
+async def generate_excel(
+    request: Request,
+    datum: str = Form(...),
+    projekt: str = Form(...),
+    geraet: str = Form(""),
+    mitarbeiter1: str = Form(...),
+    stunden1: str = Form(...),
+):
+    workbook = openpyxl.load_workbook("GP-t.xlsx")
+    sheet = workbook.active
+
+    sheet["C6"] = datum
+    sheet["C7"] = projekt
+    sheet["C8"] = geraet
+    sheet["C11"] = mitarbeiter1
+    sheet["L11"] = float(stunden1.replace(",", "."))
+
+    today = datetime.today().strftime("%Y-%m-%d")
+    filename = f"arbeitsnachweis_{today}.xlsx"
+    workbook.save(filename)
+
+    return FileResponse(path=filename, filename=filename, media_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
