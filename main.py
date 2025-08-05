dc0c69d1a6d1728b962357cf3d70f3ab91bd4ff9
laszlonaptar <bloffolo_szuleszno2v@icloud.com>
2025-08-05 15:04:04 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,15 +1,10 @@
-
-from flask import Flask, send_from_directory
+from flask import Flask, render_template
 
 app = Flask(__name__, static_folder='static', template_folder='templates')
 
 @app.route('/')
 def index():
-    return send_from_directory('templates', 'index.html')
-
-@app.route('/<path:path>')
-def static_files(path):
-    return send_from_directory('static', path)
+    return render_template('index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
