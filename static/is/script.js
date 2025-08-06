41e1001d5e15475a72665c04d16f4539a0928782
laszlonaptar <bloffolo_szuleszno2v@icloud.com>
2025-08-06 08:50:28 +0200

    Add files via upload

--- static/is/script.js
+++ static/is/script.js
@@ -1,1 +1,19 @@
 
+function addMitarbeiter() {
+    const container = document.getElementById('mitarbeiter-container');
+    const mitarbeiter = document.createElement('div');
+    mitarbeiter.classList.add('mitarbeiter');
+    mitarbeiter.innerHTML = `
+        <label>Nachname:</label>
+        <input type="text" name="nachname[]">
+        <label>Vorname:</label>
+        <input type="text" name="vorname[]">
+        <label>Ausweis-Nr.:</label>
+        <input type="text" name="ausweis[]">
+        <label>Beginn:</label>
+        <input type="time" name="beginn[]">
+        <label>Ende:</label>
+        <input type="time" name="ende[]">
+    `;
+    container.appendChild(mitarbeiter);
+}
