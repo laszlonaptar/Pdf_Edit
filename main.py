9b407af9aa7e4a28a4d5eb542b6eb15424ee5951
laszlonaptar <bloffolo_szuleszno2v@icloud.com>
2025-08-07 20:55:28 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,17 +1,15 @@
-
 from fastapi import FastAPI, Form, Request
-from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
-from datetime import datetime
-import openpyxl
 import shutil
+from openpyxl import load_workbook
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory="static"), name="static")
 templates = Jinja2Templates(directory="templates")
 
-@app.get("/", response_class=HTMLResponse)
+@app.get("/")
 async def form_get(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
@@ -21,51 +19,48 @@ async def generate_excel(
     datum: str = Form(...),
     projekt: str = Form(...),
     bf: str = Form(...),
-    geraet: str = Form(""),
-    was_gemacht: str = Form(...),
+    beschreibung: str = Form(...),
     name1: str = Form(...),
     vorname1: str = Form(...),
     ausweis1: str = Form(...),
     beginn1: str = Form(...),
-    ende1: str = Form(...),
+    ende1: str = Form(...)
 ):
-
     shutil.copy("GP-t.xlsx", "output.xlsx")
-    wb = openpyxl.load_workbook("output.xlsx")
+    wb = load_workbook("output.xlsx")
     ws = wb.active
 
-    # Cellákba írás (nem összevont cellába!)
-    ws["D6"].value = datum
-    ws["D7"].value = projekt
-    ws["D8"].value = bf
-    ws["D9"].value = geraet
+    # Egyszerű mezők
+    ws["D6"] = datum
+    ws["D7"] = projekt
+    ws["D8"] = bf
+    ws["A6"] = beschreibung
 
-    # "Was wurde gemacht?" mező: egy cellába (A6)
-    ws["A6"].value = was_gemacht
+    ws["A17"] = name1
+    ws["B17"] = vorname1
+    ws["C17"] = ausweis1
+    ws["D17"] = beginn1
+    ws["E17"] = ende1
 
-    # Munkaóra számítás
-    def berechne_stunden(beginn, ende):
-        fmt = "%H:%M"
-        start = datetime.strptime(beginn, fmt)
-        end = datetime.strptime(ende, fmt)
-        total = (end - start).seconds / 3600
+    # Összóra számítás szünetek levonásával
+    from datetime import datetime
 
-        # szünetek levonása
-        if start <= datetime.strptime("09:00", fmt) <= end:
-            total -= 0.25
-        if start <= datetime.strptime("12:00", fmt) <= end:
-            total -= 0.75
-        return max(total, 0)
+    def parse_time(t):
+        return datetime.strptime(t, "%H:%M")
 
-    gesamtstunden = berechne_stunden(beginn1, ende1)
+    start = parse_time(beginn1)
+    end = parse_time(ende1)
+    total_hours = (end - start).seconds / 3600
 
-    # Dolgozói adatok (első dolgozó)
-    ws["A17"].value = name1
-    ws["B17"].value = vorname1
-    ws["C17"].value = ausweis1
-    ws["E17"].value = beginn1
-    ws["F17"].value = ende1
-    ws["G17"].value = gesamtstunden
+    break_time = 0.0
+    if start <= parse_time("09:15") and end >= parse_time("09:00"):
+        break_time += 0.25
+    if start <= parse_time("12:45") and end >= parse_time("12:00"):
+        break_time += 0.75
+
+    total_hours -= break_time
+    ws["F17"] = round(total_hours, 2)
+    ws["G17"] = round(total_hours, 2)
 
     wb.save("output.xlsx")
     return FileResponse("output.xlsx", media_type="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet", filename="Arbeitsnachweis.xlsx")
