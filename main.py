a9d549724708cf400e636e7a56f73a3083b68ae6
laszlonaptar <bloffolo_szuleszno2v@icloud.com>
2025-08-14 19:20:30 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,59 +1,89 @@
+from fastapi import FastAPI, Request, Form
+from fastapi.responses import FileResponse, HTMLResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
 from reportlab.pdfgen import canvas
 from reportlab.lib.pagesizes import A4
 from reportlab.lib.utils import ImageReader
-from reportlab.pdfbase import pdfmetrics
-from reportlab.pdfbase.ttfonts import TTFont
+import io
+import os
+from datetime import datetime
 
-# Betűtípus regisztrálása
-pdfmetrics.registerFont(TTFont('Helvetica', 'Helvetica.ttf'))
+app = FastAPI()
 
-# Állandó eltolás
-OFFSET_X = 25  # px eltolás jobbra
-OFFSET_Y = 0   # ha felfelé/lefelé kellene tolni, itt állítható
+# Statikus mappák és sablonok
+app.mount("/static", StaticFiles(directory="static"), name="static")
+templates = Jinja2Templates(directory="templates")
 
-def generate_pdf(output_filename, background_image, text_data):
-    c = canvas.Canvas(output_filename, pagesize=A4)
-    width, height = A4
+@app.get("/", response_class=HTMLResponse)
+async def index(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
 
-    # Háttérkép betöltése és rajzolása eltolással
-    img = ImageReader(background_image)
+@app.post("/generate_pdf")
+async def generate_pdf(
+    request: Request,
+    datum: str = Form(...),
+    projekt: str = Form(...),
+    bf: str = Form(...),
+    taetigkeiten: str = Form(...)
+):
+    buffer = io.BytesIO()
+
+    # Oldal beállítása
+    c = canvas.Canvas(buffer, pagesize=A4)
+    page_width, page_height = A4
+
+    # Háttérkép betöltése
+    img_path = os.path.join("static", "arbeitsnachweis_bg.jpg")
+    img = ImageReader(img_path)
+
     img_width, img_height = img.getSize()
 
-    # A képet méretarányosan skálázzuk az A4 méretre
-    scale_x = width / img_width
-    scale_y = height / img_height
-    scale = min(scale_x, scale_y)
+    # Arány megtartása
+    aspect = img_height / float(img_width)
+    new_width = page_width
+    new_height = new_width * aspect
 
-    new_width = img_width * scale
-    new_height = img_height * scale
+    # Kép pozíció (középen + 25px jobbra tolás)
+    x_pos = 0
+    y_pos = page_height - new_height
+    c.drawImage(
+        img,
+        x_pos + 25,  # 25px jobbra tolás
+        y_pos,
+        width=new_width,
+        height=new_height
+    )
 
-    # Bal alsó sarok pozíciója eltolással
-    x_pos = ((width - new_width) / 2) + OFFSET_X
-    y_pos = ((height - new_height) / 2) + OFFSET_Y
-
-    c.drawImage(img, x_pos, y_pos, width=new_width, height=new_height)
-
-    # Szöveg rajzolása – minden X koordinátához hozzáadjuk az OFFSET_X-et
+    # Betűtípus
     c.setFont("Helvetica", 10)
 
-    for item in text_data:
-        text = item['text']
-        x = item['x'] + OFFSET_X
-        y = item['y'] + OFFSET_Y
-        c.drawString(x, y, text)
+    # Dátum
+    c.drawString(470, 550, datum)
+
+    # Projekt
+    c.drawString(100, 520, projekt)
+
+    # BF
+    c.drawString(470, 520, bf)
+
+    # Tevékenység leírás (több sorba tördelve, ha kell)
+    text_obj = c.beginText()
+    text_obj.setTextOrigin(40, 480)
+    text_obj.setFont("Helvetica", 10)
+    for line in taetigkeiten.split("\n"):
+        text_obj.textLine(line)
+    c.drawText(text_obj)
 
     c.showPage()
     c.save()
 
-# Tesztadatok
-text_data = [
-    {"text": "Bau und Ausführungsort: M715", "x": 100, "y": 700},
-    {"text": "Datum: 14.08.2025", "x": 400, "y": 700},
-    {"text": "Lorem ipsum dolor sit amet", "x": 100, "y": 650}
-]
+    buffer.seek(0)
 
-generate_pdf(
-    output_filename="output.pdf",
-    background_image="background.jpg",  # háttérkép elérési útja
-    text_data=text_data
-)
+    filename = f"Arbeitsnachweis_{datetime.now().strftime('%Y%m%d_%H%M%S')}.pdf"
+
+    return FileResponse(
+        buffer,
+        media_type='application/pdf',
+        filename=filename
+    )
